81cef6e975411c0e8f5bbdda1acb4bfee8d2c1ad
Zenirr <zenirr@mail.ru>
2022-10-05 05:10:31 +0600

    solved task

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,25 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string [] stringsOld = new string []{"Вы","третий","в","очереди","tri"};
+            string [] stringsNew = new string [stringsOld.Length];
+            int count = 0;
+
+            for (int i = 0; i < stringsOld.Length; i++)
+            {   
+                
+                if(stringsOld[i].Length<=3)
+                {
+                    stringsNew[count] = stringsOld[i];
+                    count++;
+                }
+            }
+
+            foreach (string line in stringsNew)
+            {
+                if(line != null)
+                Console.Write("["+line+"] ");
+            }
         }
     }
 }
